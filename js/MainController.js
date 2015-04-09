368f6435a40393b040b51ce85fec05d2d85a1e76
Brian Kelsey <brian.kelsey2@gmail.com>
2015-04-09 14:35:05 -0400

    Logic moved to cellData service. Cells now display as black block instead of x, start button automates process instead of stepping each click

--- js/MainController.js
+++ js/MainController.js
@@ -1,73 +1,28 @@
-app.controller("MainController", ['$scope', function($scope) {
-  $scope.cells = [];
-  for (var i = 0; i < 20; i++) {
-    row = [];
-    for (var j = 0; j < 20; j++) {
-      cell = {
-        index: j,
-        row: i,
-        is_alive: false,
-        display: ''
-      }
-      if (Math.random() > 0.90) {
-        cell.display = 'X';
-        cell.is_alive = true;
-      }
-      row.push(cell);
-    }
-    $scope.cells.push(row);
-  }
+app.controller("MainController", ['$scope', '$interval', 'cellData', function($scope, $interval, cellData) {
+  $scope.cellData = cellData;
+  cellData.createCells();
+  $scope.cellWidth = cellData.width;
+  $scope.cellHeight = cellData.height;
 
-  $scope.copy = [];
-
+  var stop;
   $scope.step = function() {
-    angular.copy($scope.cells, $scope.copy);
-    for (var i = 0; i < 20; i++) {
-      for (var j = 0; j < 20; j++) {
-        $scope.check_cells(i, j);
-      }
+    if (angular.isDefined(stop) == false) {
+        stop = $interval(function() {
+          if (cellData.alive_count > 0) {
+            cellData.step();
+          }
+          else {
+            $scope.stopStep();
+          }
+        }, 500);
     }
-  }
+  };
 
-  $scope.check_cells = function(row, col) {
-    total = 0;
-    // Check above
-    total += $scope.cell_alive(row-1, col-1) ? 1 : 0;
-    total += $scope.cell_alive(row-1, col) ? 1 : 0;
-    total += $scope.cell_alive(row-1, col+1) ? 1 : 0;
+  $scope.stopStep = function() {
+    if (angular.isDefined(stop)) {
+      $interval.cancel(stop)
+      stop = undefined;
+    }
+  };
 
-    // Check same row
-    total += $scope.cell_alive(row, col-1) ? 1 : 0;
-    total += $scope.cell_alive(row, col+1) ? 1 : 0;
-
-    // Check below
-    total += $scope.cell_alive(row+1, col-1) ? 1 : 0;
-    total += $scope.cell_alive(row+1, col) ? 1 : 0;
-    total += $scope.cell_alive(row+1, col+1) ? 1 : 0;
-
-    if ($scope.cells[row][col].is_alive) {
-      if (total < 2 || total > 3) {
-        $scope.cells[row][col].is_alive = false;
-        $scope.cells[row][col].display = '';
-      }
-    }
-    else {
-      if (total === 3) {
-        $scope.cells[row][col].is_alive = true;
-        $scope.cells[row][col].display = 'X';
-      }
-    }
-  }
-
-  $scope.cell_alive = function(row, col) {
-    if (row < 0 || row >= 20) {
-      return false;
-    }
-    else if (col < 0 || col >= 20) {
-      return false;
-    }
-    else {
-      return $scope.copy[row][col].is_alive
-    }
-  }
 }]);
---
+++ js/cellData.js
@@ -0,0 +1,89 @@
+app.factory('cellData', ['$http', function($http) {
+  var cellData = {
+    alive_count: 0,
+    width: 20,
+    height: 20,
+    cells: [],
+    copy: []
+  };
+
+  cellData.setWidth = function(width) {
+    cellData.width = width;
+  };
+
+  cellData.setHeight = function(height) {
+    cellData.height = height;
+  };
+
+  cellData.createCells = function() {
+    cellData.cells = [];
+    for (var i = 0; i < cellData.height; i++) {
+      var row = [];
+      for (var j = 0; j < cellData.width; j++) {
+        var cell = {
+          is_alive: false
+        }
+        if (Math.random() > 0.90) {
+          cell.is_alive = true;
+          cellData.alive_count += 1;
+        }
+        row.push(cell);
+      }
+      cellData.cells.push(row);
+    }
+  };
+
+
+  cellData.step = function() {
+    angular.copy(cellData.cells, cellData.copy);
+    for (var i = 0; i < cellData.height; i++) {
+      for (var j = 0; j < cellData.width; j++) {
+        cellData.check_cells(i, j);
+      }
+    }
+  };
+
+  cellData.check_cells = function(row, col) {
+    var total = 0;
+    // Check above
+    total += cellData.cell_alive(row-1, col-1) ? 1 : 0;
+    total += cellData.cell_alive(row-1, col) ? 1 : 0;
+    total += cellData.cell_alive(row-1, col+1) ? 1 : 0;
+
+    // Check same row
+    total += cellData.cell_alive(row, col-1) ? 1 : 0;
+    total += cellData.cell_alive(row, col+1) ? 1 : 0;
+
+    // Check below
+    total += cellData.cell_alive(row+1, col-1) ? 1 : 0;
+    total += cellData.cell_alive(row+1, col) ? 1 : 0;
+    total += cellData.cell_alive(row+1, col+1) ? 1 : 0;
+
+    if (cellData.cells[row][col].is_alive) {
+      if (total < 2 || total > 3) {
+        cellData.cells[row][col].is_alive = false;
+        cellData.alive_count -= 1;
+      }
+    }
+    else {
+      if (total === 3) {
+        cellData.cells[row][col].is_alive = true;
+        cellData.alive_count += 1;
+      }
+    }
+  };
+
+  cellData.cell_alive = function(row, col) {
+    if (row < 0 || row >= cellData.height) {
+      return false;
+    }
+    else if (col < 0 || col >= cellData.width) {
+      return false;
+    }
+    else {
+      return cellData.copy[row][col].is_alive
+    }
+  };
+
+  return cellData;
+}]);
